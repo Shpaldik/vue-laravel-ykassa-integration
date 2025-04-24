cba3e3e7f86f3eca39da81ceda99d8510388dd30
Shpaldik <shpappy7@gmail.com>
2025-04-24 14:42:32 +0500

    re-captcha validations

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -6,54 +6,34 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules;
 use Inertia\Inertia;
 use Inertia\Response;
-use App\Rules\Recaptcha;
+use App\Http\Requests\Auth\RegisterRequest;
 
 class RegisteredUserController extends Controller
 {
-    /**
-     * Display the registration view.
-     */
     public function create(): Response
     {
         return Inertia::render('Auth/Register');
     }
 
-    /**
-     * Handle an incoming registration request.
-     *
-     * @throws \Illuminate\Validation\ValidationException
-     */
-    public function store(Request $request): RedirectResponse
+    public function store(RegisterRequest $request): RedirectResponse
     {
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
-            'captcha_token'  => [new Recaptcha],
-        ]);
-
         $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
+            'name'       => $request->name,
+            'email'      => $request->email,
+            'password'   => Hash::make($request->password),
             'ip_address' => $request->ip(),
-            'status' => 0,
-            'balance' => 0,
+            'status'     => 0,
+            'balance'    => 0,
         ]);
 
         $user->sendEmailVerificationNotification();
-        
-        \Log::info('User IP Address: '.$request->ip());
-
+        Log::info('User IP Address: ' . $request->ip());
         event(new Registered($user));
-
         Auth::login($user);
 
         return redirect(route('dashboard', absolute: false));
--- app/Http/Requests/Auth/RegisterRequest.php
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -1,10 +1,12 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\Auth;
+
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Validation\Rules\Password;
 
 class RegisterRequest extends FormRequest
 {
@@ -13,7 +15,7 @@ class RegisterRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +26,26 @@ class RegisterRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name'             => 'required|string|max:255',
+            'email'            => 'required|string|email|unique:users,email',
+            'password'         => ['required','confirmed', Password::defaults()],
+            'captcha_token'    => 'required|string',
         ];
+    }
+
+    protected function passedValidation()
+    {
+        $resp = Http::asForm()->post(config('services.google_recaptcha.url'), [
+            'secret' => config('services.google_recaptcha.secret'),
+            'response' => $this->captcha_token,
+            'remoteip' => $this->ip(),
+            
+        ])->json();
+
+        if(!($resp['success'] ?? false) || ($resp['score'] ?? 0) < 0.5){
+            throw ValidationException::withMessages([
+                'captcha_token' => 'Captcha Failed. Please try again',
+            ]);
+        }
     }
 }
